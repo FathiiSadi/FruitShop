7ca152804ef924d12dffa47fbee311af983fca1d
fathi.alsadi <fathi.sadi@altibbi.com>
2026-01-02 01:01:50 +0300

    fix: hardcode database connection to ignore hidden environment variables

--- config/db.php
+++ config/db.php
@@ -2,13 +2,14 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => env('DB_DSN', 'pgsql:host=dpg-d5begcjuibrs73ce8lag-a.virginia-postgres.render.com;port=5432;dbname=fruitshop_db;sslmode=require'),
-    'username' => env('DB_USERNAME', 'fruitadmin'),
-    'password' => env('DB_PASSWORD', '6otaPMavrBeSxd7NayEGdP47NEEeuCaJ'),
+    'dsn' => 'pgsql:host=dpg-d5begcjuibrs73ce8lag-a.virginia-postgres.render.com;port=5432;dbname=fruitshop_db;sslmode=require',
+    'username' => 'fruitadmin',
+    'password' => '6otaPMavrBeSxd7NayEGdP47NEEeuCaJ',
     'charset' => 'utf8',
 
     // Essential for Render.com PostgreSQL Production
     'attributes' => [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_PERSISTENT => false,
     ],
 ];
