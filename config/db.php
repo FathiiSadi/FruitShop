0d31ca16dfc1c214802aa5871b3bcb2e178188a2
fathi.alsadi <fathi.sadi@altibbi.com>
2026-01-02 00:43:10 +0300

    fix

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => env('DB_DSN', 'pgsql:host=dpg-d5begcjuibrs73ce8lag-a;port=5432;dbname=fruitshop_db'),
+    'dsn' => env('DB_DSN', 'pgsql:host=dpg-d5begcjuibrs73ce8lag-a.virginia-postgres.render.com;port=5432;dbname=fruitshop_db;sslmode=require'),
     'username' => env('DB_USERNAME', 'fruitadmin'),
     'password' => env('DB_PASSWORD', '6otaPMavrBeSxd7NayEGdP47NEEeuCaJ'),
     'charset' => 'utf8',
