478f49d5615a604b2a0328cedbfb3db5c00f2b74
fathi.alsadi <fathi.sadi@altibbi.com>
2026-01-02 00:47:43 +0300

    fix

--- config/db.php
+++ config/db.php
@@ -2,12 +2,12 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => env('DB_DSN', 'pgsql:host=dpg-d5begcjuibrs73ce8lag-a.virginia-postgres.render.com;port=5432;dbname=fruitshop_db;sslmode=require'),
+    'dsn' => env('DB_DSN', 'pgsql:host=dpg-d5begcjuibrs73ce8lag-a;port=5432;dbname=fruitshop_db;sslmode=disable'),
     'username' => env('DB_USERNAME', 'fruitadmin'),
     'password' => env('DB_PASSWORD', '6otaPMavrBeSxd7NayEGdP47NEEeuCaJ'),
     'charset' => 'utf8',
 
-    // Essential for Render.com PostgreSQL
+    // Essential for Render.com PostgreSQL internal connections
     'attributes' => [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     ],
